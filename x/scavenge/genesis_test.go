9a90be50afdab72215f9c9e8468d09bbd78c500c
dbtcs1 <tmapl@singnet.com.sg>
2022-07-21 03:59:32 +0000

    First commit Scanvenger

---
+++ x/scavenge/genesis_test.go
@@ -0,0 +1,47 @@
+package scavenge_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "github.com/username/scavenge/testutil/keeper"
+	"github.com/username/scavenge/testutil/nullify"
+	"github.com/username/scavenge/x/scavenge"
+	"github.com/username/scavenge/x/scavenge/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		ScavengeList: []types.Scavenge{
+			{
+				Index: "0",
+			},
+			{
+				Index: "1",
+			},
+		},
+		CommitList: []types.Commit{
+			{
+				Index: "0",
+			},
+			{
+				Index: "1",
+			},
+		},
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.ScavengeKeeper(t)
+	scavenge.InitGenesis(ctx, *k, genesisState)
+	got := scavenge.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	require.ElementsMatch(t, genesisState.ScavengeList, got.ScavengeList)
+	require.ElementsMatch(t, genesisState.CommitList, got.CommitList)
+	// this line is used by starport scaffolding # genesis/test/assert
+}
